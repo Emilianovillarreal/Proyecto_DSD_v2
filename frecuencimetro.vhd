8c0f689765e1d7c46607febdb76e0e8d9b9905f3
Emiliano Villarreal <emiliano02villarreal@gmail.com>
2022-11-11 22:38:20 -0300

    Bloque principal- Agregue bloque para obtener la señal desplazada

--- frecuencimetro.vhd
+++ frecuencimetro.vhd
@@ -17,6 +17,8 @@ architecture Behavioral of frecuencimetro is
 --signal frecuencia : std_logic_vector(29 downto 0);
 
 singal s_U1_to_U2: std_logic_vector(14 downto 0);
+signal s_shifted_signal : std_logic_vector(9 downto 0);
+signal s_frecuencia_SM : std_logic_vector(9 downto 0);
 
  
 --FALTARIA REVISAR QUE TIENE DE MAL EL BLOQUE 2 
@@ -29,6 +31,22 @@ port map
 	CLK => CLK,
 	RST => RST, 
 	SALIDA  => s_U1_to_U2);
+
+U2: process(CLK, RST, s_U1_to_U2) -- se hace la division, despreciando los bits que son 0, dejando 10 bits para la posterior multiplicacion
+begin
+	if rst = '1' then 
+		s_shifted_signal <= (others => '0');
+	elsif rising_edge(clk)then 
+		s_shifted_signal <= s_U1_to_U2(14 downto 5);
+	end if;
+end process;
+
+U3: process(clk, rst, s_U1_to_U2,s_frecuencia_SM)
+begin
+	if rising_edge(clk)then 
+		s_frecuencia_SM <= s_shifted_signal;
+	end if;
+end process;
 
 
 	
