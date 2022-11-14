82538f523e8f5fabcbc7e246266173f18414f470
Emiliano Villarreal <emiliano02villarreal@gmail.com>
2022-11-13 23:53:36 -0300

    Bloque Principal: FUNCIONAL

--- frecuencimetro.vhd
+++ frecuencimetro.vhd
@@ -13,7 +13,7 @@ end frecuencimetro;
 
 architecture Behavioral of frecuencimetro is
 
-signal s_count_reach, aux : std_logic;
+signal s_count_reach,s_count_reach2, aux : std_logic;
 
 signal s_count,s_count_freeze: std_logic_vector(13 downto 0);
 signal s_frequency : std_logic_vector(14 downto 0);
@@ -30,8 +30,15 @@ port map
 	o_count_reach => s_count_reach
 );
 
+FLIP_FLOP1: process(clk, s_count_reach)
+begin
+	if rising_edge(clk) then 
+		s_count_reach2 <= s_count_reach;
+	end if;
+end process;
 
-aux <= s_count_reach or RST;
+
+aux <= s_count_reach2 or RST;
 
 
 Count_square_sign_pulses: entity work.CONTADOR_2
@@ -40,8 +47,6 @@ port map
 	i_square => I_SEÑAL,
 	o_count => s_count
 );
-
---ESTE LO DESCONECTE PORQUE SINO ME TIRA INDETERMINACIONN
 
 flip_flop:process(clk,s_count_reach)
 begin
@@ -54,8 +59,8 @@ frequency_calculation: entity work.DESPLAZAMIENTO_IZQ_V3
 port map
 (	CLK => CLK,
 	RST => RST,
-	--count_frezze =>s_count_freeze,
-	count_frezze => s_count,
+	count_frezze =>s_count_freeze,
+	--count_frezze => s_count,
 	Frequency => s_frequency
 );
 
