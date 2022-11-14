4fe47bf99fe90630c20e231a1f709d0aa79e186f
Emiliano Villarreal <emiliano02villarreal@gmail.com>
2022-11-14 00:00:58 -0300

    TestBench: FUNCIONAL

--- tb.vhd
+++ tb.vhd
@@ -1,30 +1,3 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   22:28:33 11/12/2022
--- Design Name:   
--- Module Name:   C:/EMILIANO/PROYECTO_DSD_V2/tb.vhd
--- Project Name:  PROYECTO_DSD_V2
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: frecuencimetro
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
